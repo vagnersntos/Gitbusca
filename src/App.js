e39dfd62845ac33bb731ab8435e63caab852a76d
Nelcivagner Santos <nelcivagner.santos@prestadores.uhgbrasil.com.br>
2022-06-20 18:39:31 -0300

    Busca de usuários no github

--- src/App.js
+++ src/App.js
@@ -1,22 +1,92 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
+import service from './services/services';
+import {
+  Button,
+  InputGroup,
+  Input,
+  Table
+} from 'reactstrap';
 
 function App() {
+
+  const [user, setUser] = useState();
+  const [name, setName] = useState('');
+  const [button, setButton] = useState("success");
+  const [stylleInput, setStylleInput] = useState(false);
+
+  const getUsers = () => {
+    if (name || name !== '') {
+      service.get(`/users/${name}`)
+        .then((response) =>
+          setUser(response.data),
+          setButton("success"),
+          setStylleInput(false)
+        )
+        .catch((err) => {
+          console.log("Ocorreu um Erro!" + err)
+          if (err) apllyDanger();
+        })
+    }
+  }
+
+  const handleChange = (item) => {
+    setName(item);
+  };
+
+  const apllyDanger = () => {
+    setUser('')
+    setStylleInput(true)
+    setButton("danger")
+    return
+  };
+
+  const handlereset = () => {
+    document.querySelectorAll("input.form-control").forEach(input => (input.value = ''))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+            <div>
+          <div>
+            <h3>Git Busca</h3>
+          </div>
+            <InputGroup>
+              <Button color={button} onClick={() => { handlereset() }}>
+                Limpar
+              </Button>
+              <Input invalid={stylleInput} placeholder="Nome do Usuário..." onChange={(e) => handleChange(e.target.value)} />
+              <Button data-bs-toggle="tooltip" data-bs-placement="right" title={stylleInput ? 'Usuário não encontrado' : ''} color={button} onClick={() => { getUsers() }}>
+                Buscar
+              </Button>
+            </InputGroup>
+          </div>
+          {user && <Table
+            responsive
+            size="sm"
+          >
+            <thead>
+              <tr>
+                <th>
+                  Usuário:
+                </th>
+                <th>
+                  {user?.name}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>
+                  Perfil GitHub:
+                </th>
+                <td>
+                  <a href={user?.html_url} target="_blanlk">{user?.html_url}</a>
+                </td>
+              </tr>
+            </tbody>
+          </Table>}
       </header>
     </div>
   );
